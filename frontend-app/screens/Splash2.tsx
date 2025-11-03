ab1ed4e0b223b0dfc1f18ba80251328c8d15a02c
karla12-td <ktrejo@anahuac.mx>
2025-11-03 14:16:53 -0500

    Splash

--- frontend-app/screens/Splash2.tsx
+++ frontend-app/screens/Splash2.tsx
@@ -1,34 +1,58 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import {
   View,
   Text,
   StyleSheet,
-  SafeAreaView,
   ActivityIndicator,
   Image,
+  Animated, 
 } from "react-native";
 
 export default function Splash() {
   const appLogo = require("../assets/logominimal.png");
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+ 
+  useEffect(() => {
+    Animated.timing(
+      fadeAnim, 
+      {
+        toValue: 1, 
+        duration: 1500, 
+        useNativeDriver: true, 
+      }
+    ).start(); 
+  }, [fadeAnim]); 
+
   return (
-    
-      <View style={styles.centeredContent}>
-        <Image source={appLogo} style={styles.logo} resizeMode="contain" />
+    <View style={styles.centeredContent}>
+      
+      <Animated.Image
+        source={appLogo}
+        style={[styles.logo, { opacity: fadeAnim }]} 
+        resizeMode="contain"
+      />
 
-        <Text style={styles.title}>Erase una vez</Text>
+      
+      <Animated.Text style={[styles.title, { opacity: fadeAnim }]}>
+        Erase una vez
+      </Animated.Text>
 
+      
+      <Animated.View style={[{ opacity: fadeAnim }]}>
         <ActivityIndicator
           size="small"
           color="#888888"
           style={styles.spinner}
         />
-      </View>
-   
+      </Animated.View>
+    </View>
   );
 }
 
+// 
 const styles = StyleSheet.create({
-  
   centeredContent: {
     flex: 1,
     backgroundColor: "#FFFBF5",
@@ -40,11 +64,11 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: "bold",
     color: "#333333",
-    marginTop: 24, 
+    marginTop: 24,
     textAlign: "center",
   },
   spinner: {
-    marginTop: 20, 
+    marginTop: 20,
   },
   logo: {
     width: 150,
